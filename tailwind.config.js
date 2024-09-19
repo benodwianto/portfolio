49f5192a520a24f9bf5e27fc746dc28bece33c51
Beno Dwianto <benodwianto9@gmail.com>
2024-09-19 15:17:08 +0700

    menambahkan animasi flipcard pada bagian stack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,22 @@
+const plugin = require('tailwindcss/plugin');
+
+const Myclass = plugin(function({ addUtilities }) {
+  addUtilities({
+    '.my-rotate-y-180': {
+      transform: 'rotateY(180deg)',
+    },
+    '.preserve-3d': {
+      transformStyle: 'preserve-3d',
+    },
+    '.perspective': {
+      perspective: '1000px',
+    },
+    '.backface-hidden': {
+      backfaceVisibility: 'hidden',
+    },
+  });
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/**/**.{html,js}'],
@@ -23,10 +42,10 @@ module.exports = {
         upDown2: {
           '0%, 100%': { transform: 'translateY(0)' }, // Posisi awal dan akhir tetap
           '50%': { transform: 'translateY(20px)' },   // Gerak ke bawah dulu (10px)
-        },
+        },        
       }
     },
   },
-  plugins: [],
+  plugins: [Myclass],
 } 
 
